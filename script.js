cb1ff2a1db09a29b87a2773e77b9c426d44a2b2c
minsapps <menachemgrumet@gmail.com>
2025-11-21 12:59:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// intentionally left empty – all logic is now inside individual HTML pages.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOjQmJmvfG_iK62GIl_UgiTcv8J_Y1Dv0",
+  authDomain: "construction-management-f48cb.firebaseapp.com",
+  projectId: "construction-management-f48cb",
+  storageBucket: "construction-management-f48cb.firebasestorage.app",
+  messagingSenderId: "1013084592434",
+  appId: "1:1013084592434:web:79226b78462dbfbd519f2b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
